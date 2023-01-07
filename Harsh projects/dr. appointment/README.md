b31a55d34a94daea120d04c02eb554082e841698
Harsh Gupta <harshgupta0209@gmail.com>
2023-01-07 12:02:50 +0530

    task 1 complete

---
+++ Harsh projects/dr. appointment/README.md
@@ -0,0 +1,15 @@
+# DoctorAppointmentProject
+This code was generated using AnimaApp.com. 
+Anima allows teams to innovate faster by converting design to code, automatically.
+
+## Are you a developer?
+This code is a high-fidelity prototype.
+Get developer-friendly React or HTML/CSS code for this project at:
+https://projects.animaapp.com/team/hackveda-6p4fqix/project/ASMyVeE?utm_source=package-code
+You might need to request access.
+
+## Got feedback?
+Feedback on code quality is welcome at code@animaapp.com.
+
+Have a creative day,
+Anima team
---
+++ Harsh projects/dr. appointment/webservice.php
@@ -0,0 +1,54 @@
+<?php 
+
+$search_param = $_POST["search"];
+$search_area = $_POST["area"];
+//connect to data base
+if(isset($_POST["search"]) && isset($_POST["area"])){
+    echo $search_param;
+    echo $search_area;
+$host="localhost";
+$dbuser="id20068504_doctor";
+$dbpass="Aniket@545454";
+$dbname="id20068504_doctorinfo";
+
+//make connection
+$conn = new mysqli($host,$dbuser,$dbpass,$dbname);
+
+$sql = "SELECT * FROM `doctors` WHERE DoctorArea LIKE '%".$search_area."%' and DoctorCategory LIKE '%".$search_param."%' " ;
+
+$result = $conn->query($sql);
+
+if($result -> num_rows > 0){
+    $data = '<div class="lbl-title-section3-WYJxxj">Doctor Found</div>' ;
+    $doctor_data= "" ;
+while($row = $result->fetch_assoc()){
+    $doctorid = $row["ID"];
+    $doctorname =$row["DoctorName"];
+    $doctorinfo =$row["DoctorInformation"];
+    $doctorimage= $row["DoctorImage"];
+   
+   $doctor_data = $doctor_data.'<div class="search-section-WYJxxj smart-layers-pointers">
+            <div class="search-box-vYzqLm">
+              <div class="search-bg-gDPMPr"></div>
+              <img class="search-icon-gDPMPr" src="'.$doctorimage.'" />
+            </div>
+            <div class="title-search-doctor-vYzqLm roboto-bold-black-36px">'.$doctorname.'</div>
+            <div class="desc-seacrh-doctor-vYzqLm roboto-normal-black-24px">
+             '.$doctorinfo.'
+            </div>
+          </div>' ;
+}
+
+}else{
+
+$data = '<div class="lbl-title-section3-WYJxxj">No Doctor Found</div>' ;
+}
+}
+else{
+    $data = '<div class="lbl-title-section3-WYJxxj">Bad Query </div>' ;
+}
+
+$data = $data.$doctor_data;
+echo $data ;
+
+?>
